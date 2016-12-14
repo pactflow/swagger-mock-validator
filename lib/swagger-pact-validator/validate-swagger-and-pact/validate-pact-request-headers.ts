adca8bd6a3bfb659eb888e422c24f344ed1e3fa6
Ben Sayers <bsayers@atlassian.com>
2016-12-14 18:07:08 +1100

    feat: undefined non-standard request headers are now warnings

--- lib/swagger-pact-validator/validate-swagger-and-pact/validate-pact-request-headers.ts
+++ lib/swagger-pact-validator/validate-swagger-and-pact/validate-pact-request-headers.ts
@@ -3,6 +3,59 @@ import * as _ from 'lodash';
 import result from '../result';
 import {JsonSchema, ParsedMockInteraction, ParsedMockValue, ParsedSpecOperation, ParsedSpecParameter} from '../types';
 
+const standardHttpRequestHeaders = [
+    'Accept',
+    'Accept-Charset',
+    'Accept-Datetime',
+    'Accept-Encoding',
+    'Accept-Language',
+    'Authorization',
+    'Cache-Control',
+    'Connection',
+    'Content-Length',
+    'Content-MD5',
+    'Content-Type',
+    'Cookie',
+    'Date',
+    'Expect',
+    'Forwarded',
+    'From',
+    'Host',
+    'If-Match',
+    'If-Modified-Since',
+    'If-None-Match',
+    'If-Range',
+    'If-Unmodified-Since',
+    'Max-Forwards',
+    'Origin',
+    'Pragma',
+    'Proxy-Authorization',
+    'Range',
+    'Referer',
+    'TE',
+    'Upgrade',
+    'User-Agent',
+    'Via',
+    'Warning'
+];
+
+const getWarningForUndefinedHeaderOrNone = (
+    headerName: string,
+    pactHeader: ParsedMockValue<string>,
+    swaggerOperation: ParsedSpecOperation
+) => {
+    if (standardHttpRequestHeaders.indexOf(headerName) > -1) {
+        return [];
+    }
+
+    return [result.warning({
+        message: `Request header is not defined in the swagger file: ${headerName}`,
+        pactSegment: pactHeader,
+        source: 'swagger-pact-validation',
+        swaggerSegment: swaggerOperation
+    })];
+};
+
 const toJsonSchema = (parameter: ParsedSpecParameter): JsonSchema => {
     const schema: JsonSchema = {
         properties: {
@@ -32,8 +85,33 @@ const validateJson = (jsonSchema: JsonSchema, json: any) => {
     return ajv.errors;
 };
 
-const isUndefinedHeaderMocked = <T>(swaggerHeader: ParsedSpecParameter, pactHeader: ParsedMockValue<T>) =>
-    !swaggerHeader && pactHeader;
+const validateMockHeaderAgainstSpec = <T>(
+    headerName: string,
+    swaggerHeader: ParsedSpecParameter,
+    pactHeader: ParsedMockValue<T>,
+    pactInteraction: ParsedMockInteraction
+) => {
+    if (swaggerHeader.type === 'array') {
+        return [result.warning({
+            message: `Validating headers of type "${swaggerHeader.type}" are not supported, ` +
+            `assuming value is valid: ${headerName}`,
+            pactSegment: pactHeader,
+            source: 'swagger-pact-validation',
+            swaggerSegment: swaggerHeader
+        })];
+    }
+
+    const swaggerHeaderSchema = toJsonSchema(swaggerHeader);
+    const errors = validateJson(swaggerHeaderSchema, {value: (pactHeader || {value: undefined}).value});
+
+    return _.map(errors, (error) => result.error({
+        message: 'Request header is incompatible with the header parameter defined in the swagger file: ' +
+        error.message,
+        pactSegment: pactHeader || pactInteraction,
+        source: 'swagger-pact-validation',
+        swaggerSegment: swaggerHeader
+    }));
+};
 
 export default (pactInteraction: ParsedMockInteraction, swaggerOperation: ParsedSpecOperation) => {
     const allHeaders = _.union(_.keys(pactInteraction.requestHeaders), _.keys(swaggerOperation.headerParameters));
@@ -42,35 +120,11 @@ export default (pactInteraction: ParsedMockInteraction, swaggerOperation: Parsed
         const pactHeader = pactInteraction.requestHeaders[headerName];
         const swaggerHeader = swaggerOperation.headerParameters[headerName];
 
-        if (isUndefinedHeaderMocked(swaggerHeader, pactHeader)) {
-            return [result.error({
-                message: `Request header is not defined in the swagger file: ${headerName}`,
-                pactSegment: pactHeader,
-                source: 'swagger-pact-validation',
-                swaggerSegment: swaggerOperation
-            })];
+        if (!swaggerHeader && pactHeader) {
+            return getWarningForUndefinedHeaderOrNone(headerName, pactHeader, swaggerOperation);
         }
 
-        if (swaggerHeader.type === 'array') {
-            return [result.warning({
-                message: `Validating headers of type "${swaggerHeader.type}" are not supported, ` +
-                    `assuming value is valid: ${headerName}`,
-                pactSegment: pactHeader,
-                source: 'swagger-pact-validation',
-                swaggerSegment: swaggerHeader
-            })];
-        }
-
-        const swaggerHeaderSchema = toJsonSchema(swaggerHeader);
-        const errors = validateJson(swaggerHeaderSchema, {value: (pactHeader || {value: undefined}).value});
-
-        return _.map(errors, (error) => result.error({
-            message: 'Request header is incompatible with the header parameter defined in the swagger file: ' +
-            error.message,
-            pactSegment: pactHeader || pactInteraction,
-            source: 'swagger-pact-validation',
-            swaggerSegment: swaggerHeader
-        }));
+        return validateMockHeaderAgainstSpec(headerName, swaggerHeader, pactHeader, pactInteraction);
     });
 
     return _.flatten(validationErrors);
--- test/unit/swagger-pact-validator-request-headers.spec.ts
+++ test/unit/swagger-pact-validator-request-headers.spec.ts
@@ -141,14 +141,11 @@ describe('swagger-pact-validator request headers', () => {
         });
     }));
 
-    it('should fail when a pact request header is defined that is not in the spec', willResolve(() => {
+    it('should return a warning when a pact request header is defined that is not in the spec', willResolve(() => {
         const requestHeaders = {'x-custom-header': 'value'};
 
-        const result = validateRequestHeaders(null, requestHeaders);
-
-        return expectToReject(result).then((error) => {
-            expect(error).toEqual(expectedFailedValidationError);
-            (expect(error.details) as any).toContainErrors([{
+        return validateRequestHeaders(null, requestHeaders).then((result) => {
+            (expect(result) as any).toContainWarnings([{
                 message: 'Request header is not defined in the swagger file: x-custom-header',
                 pactDetails: {
                     interactionDescription: 'interaction description',
@@ -163,8 +160,50 @@ describe('swagger-pact-validator request headers', () => {
                     pathName: '/does/exist',
                     value: operationBuilder.build()
                 },
-                type: 'error'
+                type: 'warning'
             }]);
+        });
+    }));
+
+    it('should pass when pact request headers not defined in the spec are standard http headers', willResolve(() => {
+        const requestHeaders = {
+            'Accept': 'text/plain',
+            'Accept-Charset': 'utf-8',
+            'Accept-Datetime': 'Thu, 31 May 2007 20:35:00 GMT',
+            'Accept-Encoding': 'gzip',
+            'Accept-Language': 'en-US',
+            'Authorization': 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Length': '348',
+            'Content-MD5': 'Q2hlY2sgSW50ZWdyaXR5IQ==',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cookie': '$Version=1',
+            'Date': 'Tue, 15 Nov 1994 08:12:31 GMT',
+            'Expect': '100-continue',
+            'Forwarded': 'for=192.0.2.60;proto=http;by=203.0.113.43 Forwarded: for=192.0.2.43, for=198.51.100.17',
+            'From': 'user@example.com',
+            'Host': 'en.wikipedia.org:8080',
+            'If-Match': '"737060cd8c284d8af7ad3082f209582d"',
+            'If-Modified-Since': 'Sat, 29 Oct 1994 19:43:31 GMT',
+            'If-None-Match': '"737060cd8c284d8af7ad3082f209582d"',
+            'If-Range': '"737060cd8c284d8af7ad3082f209582d"',
+            'If-Unmodified-Since': 'Sat, 29 Oct 1994 19:43:31 GMT',
+            'Max-Forwards': '10',
+            'Origin': 'http://www.example-social-network.com',
+            'Pragma': 'no-cache',
+            'Proxy-Authorization': 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==',
+            'Range': 'bytes=500-999',
+            'Referer': 'http://en.wikipedia.org/wiki/Main_Page',
+            'TE': 'trailers, deflate',
+            'Upgrade': 'HTTP/2.0, HTTPS/1.3, IRC/6.9, RTA/x11, websocket',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/21.0',
+            'Via': '1.0 fred, 1.1 example.com (Apache/1.1)',
+            'Warning': '199 Miscellaneous warning'
+        };
+
+        return validateRequestHeaders(null, requestHeaders).then((result) => {
+            (expect(result) as any).toContainNoWarnings();
         });
     }));
 });
