3675fb956ef9bfd07324ad3c5206c94321c78e6a
Ben Sayers <bsayers@atlassian.com>
2018-02-19 17:59:35 +1100

    chore: bump node and dependencies

--- bin/build.sh
+++ bin/build.sh
@@ -9,19 +9,19 @@ npm install
 npm test
 
 echo ==== Test using node 6.x ====
-nvm install 6.12.2
+nvm install 6.13.0
 rm -rf node_modules
 npm install
 npm test
 
 echo ==== Test using node 8.x ====
-nvm install 8.9.3
+nvm install 8.9.4
 rm -rf node_modules
 npm install --no-shrinkwrap
 npm test
 
 echo ==== Test using node 9.x ====
-nvm install 9.3.0
+nvm install 9.5.0
 rm -rf node_modules
 npm install --no-shrinkwrap
 npm test
