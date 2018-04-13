05fdbe0ed11fe1a43404cff36e74567f6340113a
Ben Sayers <bsayers@atlassian.com>
2018-04-13 15:05:46 +1000

    chore: lint last 20 commits instead of 40

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,7 @@ gulp.task('create-new-tag', (callback) => {
 gulp.task('lint', ['lint-typescript', 'lint-commits']);
 
 gulp.task('lint-commits', () =>
-    exec('./node_modules/.bin/conventional-changelog-lint --from=HEAD~40 --preset angular')
+    exec('./node_modules/.bin/conventional-changelog-lint --from=HEAD~20 --preset angular')
 );
 
 gulp.task('lint-typescript', () => tsProjectBuildOutput.src()
