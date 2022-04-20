6acc2c17621cde3ecfde1c671ec555de01d13a2e
Yousaf Nabi <yousafn@gmail.com>
2022-04-20 16:32:13 +0100

    chore: update release scripts to use PF token for trigger

--- scripts/trigger-release-minor.sh
+++ scripts/trigger-release-minor.sh
@@ -6,17 +6,17 @@
 # Borrowed from Beth Skurrie's excellent script at
 # https://github.com/pact-foundation/pact-ruby/blob/master/script/trigger-release.sh
 
-: "${GITHUB_ACCESS_TOKEN_FOR_YOU54F_RELEASES:?Please set environment variable GITHUB_ACCESS_TOKEN_FOR_YOU54F_RELEASES}"
+: "${GITHUB_ACCESS_TOKEN_FOR_PACTFLOW_RELEASES:?Please set environment variable GITHUB_ACCESS_TOKEN_FOR_PACTFLOW_RELEASES}"
 
 repository_slug=$(git remote get-url $(git remote show) | cut -d':' -f2 | sed 's/\.git//')
 
 output=$(curl -L -v -X POST https://api.github.com/repos/${repository_slug}/dispatches \
       -H 'Accept: application/vnd.github.everest-preview+json' \
-      -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN_FOR_YOU54F_RELEASES" \
+      -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN_FOR_PACTFLOW_RELEASES" \
       -d "{\"event_type\": \"release-triggered\",\"client_payload\": {\"release_type\": \"minor\"}}" 2>&1)
 
 if  ! echo "${output}" | grep "HTTP\/2 204" > /dev/null; then
-  echo "$output" | sed  "s/${GITHUB_ACCESS_TOKEN_FOR_YOU54F_RELEASES}/********/g"
+  echo "$output" | sed  "s/${GITHUB_ACCESS_TOKEN_FOR_PACTFLOW_RELEASES}/********/g"
   echo "Failed to trigger release"
   exit 1
 else
