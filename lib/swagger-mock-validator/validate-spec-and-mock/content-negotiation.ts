b55c163cbd932cb7d474af19f319731ae6d55c6e
Voon Wong <voon.wong@smartbear.com>
2023-05-24 12:07:19 +1000

    style: fix lint

--- lib/swagger-mock-validator/validate-spec-and-mock/content-negotiation.ts
+++ lib/swagger-mock-validator/validate-spec-and-mock/content-negotiation.ts
@@ -17,7 +17,7 @@ const ignoreParameters = (t: string): string => t.split(PARAMETER_SEPARATOR)[0];
 const removeQuality = (t: string): string => t.replace(/;q=[01].?\d*/, '');
 const type = (t: string): string => t.split(TYPE_SUBTYPE_SEPARATOR)[0];
 const subtype = (t: string): string | undefined => {
-    const [_type, subtype] = t.split(TYPE_SUBTYPE_SEPARATOR);
+    const [_, subtype] = t.split(TYPE_SUBTYPE_SEPARATOR);
     return subtype?.split(EXTENSION_SEPARATOR).pop();
 };
 
@@ -25,9 +25,9 @@ export function findMatchingType(requestType: string, responseTypes: string[]): 
     // exact match
     let accept = requestType.split(',').map(ignoreWhitespace).map(ignoreCasing).sort(byQuality).map(removeQuality);
     let available = responseTypes.map(ignoreWhitespace).map(ignoreCasing);
-    for (let a of accept) {
-        let matchExactly = (t: string): boolean => t === a;
-        let index = available.findIndex(matchExactly);
+    for (const a of accept) {
+        const matchExactly = (t: string): boolean => t === a;
+        const index = available.findIndex(matchExactly);
         if (index >= 0) {
             return responseTypes[index];
         }
@@ -35,18 +35,18 @@ export function findMatchingType(requestType: string, responseTypes: string[]): 
 
     // ignore additional parameters
     accept = accept.map(ignoreParameters);
-    for (let a of accept) {
-        let matchExactly = (t: string): boolean => t === a;
-        let index = available.findIndex(matchExactly);
+    for (const a of accept) {
+        const matchExactly = (t: string): boolean => t === a;
+        const index = available.findIndex(matchExactly);
         if (index >= 0) {
             return responseTypes[index];
         }
     }
 
     // ignore vendor extensions
-    for (let a of accept) {
-        let matchTypesAndSubtypes = (t: string): boolean => type(t) === type(a) && subtype(t) === subtype(a);
-        let index = available.findIndex(matchTypesAndSubtypes);
+    for (const a of accept) {
+        const matchTypesAndSubtypes = (t: string): boolean => type(t) === type(a) && subtype(t) === subtype(a);
+        const index = available.findIndex(matchTypesAndSubtypes);
         if (index >= 0) {
             return responseTypes[index];
         }
@@ -54,9 +54,9 @@ export function findMatchingType(requestType: string, responseTypes: string[]): 
 
     // wildcards in responseTypes
     available = available.map(ignoreParameters);
-    for (let a of accept) {
-        let matchSubtype = (t: string): boolean => subtype(t) === WILDCARD && type(t) === type(a);
-        let index = available.findIndex(matchSubtype);
+    for (const a of accept) {
+        const matchSubtype = (t: string): boolean => subtype(t) === WILDCARD && type(t) === type(a);
+        const index = available.findIndex(matchSubtype);
         if (index >= 0) {
             return responseTypes[index];
         }
@@ -66,9 +66,9 @@ export function findMatchingType(requestType: string, responseTypes: string[]): 
     }
 
     // wildcards in requestTypes
-    for (let a of accept) {
-        let matchSubtype = (t: string): boolean => subtype(a) === WILDCARD && type(t) === type(a);
-        let index = available.findIndex(matchSubtype);
+    for (const a of accept) {
+        const matchSubtype = (t: string): boolean => subtype(a) === WILDCARD && type(t) === type(a);
+        const index = available.findIndex(matchSubtype);
         if (index >= 0) {
             return responseTypes[index];
         }
