8a010166239139bceb8281a5abe26a1d3b4b6f6a
Voon Wong <voon.wong@smartbear.com>
2023-08-21 10:47:06 +1000

    chore: package updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,5 +7,17 @@ module.exports = {
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'prettier',
-    ]
+    ],
+    rules: {
+        '@typescript-eslint/no-explicit-any': ['warn'],
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                args: 'after-used',
+                argsIgnorePattern: '_',
+                ignoreRestSiblings: false,
+                vars: 'all',
+            },
+        ],
+    },
 };
